ea5c133d9e4deabe12eacfaf3cf755c297cd8719
Hao Xiang Liew <haoxiangliew@gmail.com>
2026-08-18 19:18:42 -0500

    chore: flake update

--- home/darwin.nix
+++ home/darwin.nix
@@ -10,6 +10,19 @@
       package = null;
       settings.font-size = 12;
     };
+    lazydocker = {
+      enable = true;
+      settings = {
+        gui.containerStatusHealthStyle = "icon";
+        customCommands.containers = [
+          {
+            name = "orb debug";
+            attach = true;
+            command = "orb debug {{ .Container.Name }}";
+          }
+        ];
+      };
+    };
     man.generateCaches = false;
   };
 }
