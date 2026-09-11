22e6248a43eb8715df46486f9e82fc64e3bdfed7
Hao Xiang Liew <haoxiangliew@gmail.com>
2026-09-11 13:39:48 -0700

    update: config

--- devices/fluidstack.nix
+++ devices/fluidstack.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, ... }:
 
 {
   system = {
@@ -6,6 +6,11 @@
     stateVersion = 7;
   };
   users.users.hao.home = "/Users/hao";
+
+  # Endpoint-security agents on this machine (Falcon, Kandji, Code42) make
+  # link/rename syscalls ~50x slower while Nessus scans run, which stalls
+  # inline store optimisation.
+  determinateNix.customSettings.auto-optimise-store = lib.mkForce false;
   home-manager.users.hao = {
     home.stateVersion = "26.05";
   };
